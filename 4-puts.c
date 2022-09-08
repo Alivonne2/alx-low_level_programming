9711483ba056702aa8b42bf37d04566a89ec27d5
Alivonne2 <111153210+Alivonne2@users.noreply.github.com>
2022-09-08 21:18:38 +0100

    Create 4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,6 @@
+"Programming is like building a multilingual puzzle,
+ c/0x00$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out
+"Programming is like building a multilingual puzzle
+:~/c/0x00$ echo $?
+0
+:~/c/0x00$ 
