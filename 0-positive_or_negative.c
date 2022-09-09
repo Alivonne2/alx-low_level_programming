5261bf9edd8f485b516bc3a95d2f0a991319a140
Alivonne2 <111153210+Alivonne2@users.noreply.github.com>
2022-09-09 21:27:11 +0100

    Create 0-positive_or_negative.c

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,18 @@
+~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative
+:~/0x01$ ./0-positive_or_negative 
+-520693284 is negative
+:~/0x01$ ./0-positive_or_negative 
+-973398895 is negative
+:~/0x01$ ./0-positive_or_negative 
+-199220452 is negative
+:~/0x01$ ./0-positive_or_negative 
+561319348 is positive
+:~/0x01$ ./0-positive_or_negative 
+561319348 is positive
+:~/0x01$ ./0-positive_or_negative 
+266853958 is positive
+:~/0x01$ ./0-positive_or_negative 
+-48147767 is negative
+:~/0x01$ ./0-positive_or_negative 
+0 is zero
+:~/0x01$ 
