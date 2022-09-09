537be82c1a8d859bbbfbd23f31e077604f8dd9ed
Alivonne2 <111153210+Alivonne2@users.noreply.github.com>
2022-09-09 21:35:14 +0100

    Create 3-print_alphabets.c

---
+++ 3-print_alphabets.c
@@ -0,0 +1,4 @@
+:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets
+:~/0x01$ ./3-print_alphabets | cat -e
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$
+:~/0x01$ 
