41f01f880f20f96cc4a39f69c0713d0fc64a8a09
Alivonne2 <111153210+Alivonne2@users.noreply.github.com>
2022-09-09 21:36:27 +0100

    Create  4-print_alphabt.c

---
+++ 4-print_alphabt.c
@@ -0,0 +1,5 @@
+:~/0x01$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 4-print_alphabt.c -o 4-print_alphabt
+:~/0x01$ ./4-print_alphabt 
+abcdfghijklmnoprstuvwxyz
+:~/0x01$ ./4-print_alphabt | grep [eq]
+:~/0x01$ 
