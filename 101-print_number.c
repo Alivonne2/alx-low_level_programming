804efa843c9a008f1c27dfaa0ae5e3158cfdadf8
Alivonne2 <111153210+Alivonne2@users.noreply.github.com>
2022-09-15 20:21:21 +0100

    Create 101-print_number.c

---
+++ 101-print_number.c
@@ -0,0 +1,10 @@
+
+}
+:~/0x04$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 101-main.c 101-print_number.c -o 101-print_numbers
+:~/0x04$ ./101-print_numbers 
+98
+402
+1024
+0
+-98
+:~/0x04$ 
