36c7db40c787988a11f5c9cc3abdeb7e27994991
Alivonne2 <111153210+Alivonne2@users.noreply.github.com>
2022-09-19 20:33:28 +0100

    Create 3-puts.c

---
+++ 3-puts.c
@@ -0,0 +1,6 @@
+3. I do not fear computers. I fear the lack of 
+them
+~/0x05$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-puts.c -o 3-puts
+:~/0x05$ ./3-puts 
+I do not fear computers. I fear the lack of them - Isaac Asimov
+:~/0x05$ 
